8c9e1efd63a524c6d6afdb537cd22c3813a327b3
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-07 01:13:48 +0530

    Add Member and Staff controllers with CRUD operations and health check endpoints

--- src/main/java/lk/puLeeNa/LibManagement/controller/MemberController.java
+++ src/main/java/lk/puLeeNa/LibManagement/controller/MemberController.java
@@ -1,9 +1,46 @@
 package lk.puLeeNa.LibManagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.puLeeNa.LibManagement.dto.MemberDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/members")
 public class MemberController {
+    @GetMapping("health")
+    public String healthTest() {
+        return "Member Controller is Working";
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> addMember(@RequestBody MemberDTO memberDTO) {
+        System.out.println(memberDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> deleteMember(@RequestParam ("memberId") String memberId){
+        System.out.println(memberId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @PatchMapping(value = "/{memberId}", consumes =  MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> upddateMember(@PathVariable String memberId, @RequestBody MemberDTO memberDTO) {
+        System.out.println(memberId);
+        System.out.println(memberDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @GetMapping("/{memberId}")
+    public ResponseEntity<MemberDTO> getSelectedMember(@PathVariable String memberId) {
+        System.out.println(memberId);
+        MemberDTO memberDTO = new MemberDTO();
+        return new ResponseEntity<>(memberDTO, HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<MemberDTO>> getAllMembers() {
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        return new ResponseEntity<>(memberDTOList, HttpStatus.OK);
+    }
 }
--- src/main/java/lk/puLeeNa/LibManagement/controller/StaffController.java
+++ src/main/java/lk/puLeeNa/LibManagement/controller/StaffController.java
@@ -1,9 +1,48 @@
 package lk.puLeeNa.LibManagement.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lk.puLeeNa.LibManagement.dto.BookDTO;
+import lk.puLeeNa.LibManagement.dto.StaffDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/staffs")
 public class StaffController {
+    @GetMapping("health")
+    public String healthTest() {
+        return "Staff Controller is Working";
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> addStaffMember(@RequestBody StaffDTO staffDTO){
+        System.out.println(staffDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    @DeleteMapping
+    public ResponseEntity<Void> deleteStaffMember(@RequestParam ("staffId") String staffId){
+        System.out.println(staffId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        // return ResponseEntity.noContent().build();
+    }
+    @PatchMapping(value = "/{staffId}", consumes =  MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateStaffMember(@PathVariable String staffId, @RequestBody StaffDTO staffDTO){
+        System.out.println(staffId);
+        System.out.println(staffDTO);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @GetMapping("/{staffId}")
+    public ResponseEntity<StaffDTO> getSelectedStaffMember(@PathVariable String staffId){
+        System.out.println(staffId);
+        StaffDTO staffDTO = new StaffDTO();
+        return new ResponseEntity<>(staffDTO, HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<StaffDTO>> getAllStaffMembers(){
+        List<StaffDTO> staffDTOList = new ArrayList<>();
+        return new ResponseEntity<>(staffDTOList, HttpStatus.OK);
+    }
 }
