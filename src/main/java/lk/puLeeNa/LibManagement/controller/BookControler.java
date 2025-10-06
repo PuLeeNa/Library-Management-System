a6dc6c116b31879326fe6ce72d4aadadff8f8651
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-06 17:22:36 +0530

    Add BookDTO and implement BookController with health check and addBook endpoint

--- src/main/java/lk/puLeeNa/LibManagement/controller/BookControler.java
+++ src/main/java/lk/puLeeNa/LibManagement/controller/BookControler.java
@@ -1,4 +1,19 @@
 package lk.puLeeNa.LibManagement.controller;
 
+import lk.puLeeNa.LibManagement.dto.BookDTO;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/books")
 public class BookControler {
+    @GetMapping("health")
+    public String healthTest() {
+        return "Book Controller is Working";
+    }
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public BookDTO addBook(@RequestBody BookDTO bookDTO){
+        System.out.println(bookDTO);
+        return bookDTO;
+    }
 }
