b2f8277c4bffb5d188be8877d591fea1fbe9b69b
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-06 18:39:08 +0530

    Refactor BookDTO to use Lombok annotations for boilerplate reduction and update pom.xml for Lombok versioning

--- src/main/java/lk/puLeeNa/LibManagement/dto/BookDTO.java
+++ src/main/java/lk/puLeeNa/LibManagement/dto/BookDTO.java
@@ -1,7 +1,14 @@
 package lk.puLeeNa.LibManagement.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class BookDTO implements Serializable {
     private String bookId;
     private String bookName;
@@ -14,126 +21,4 @@ public class BookDTO implements Serializable {
     private Integer availableQty;
     private String lastUpdateDate;
     private String lastUpdateTime;
-
-    public BookDTO(String bookId, String bookName, String author, String edition, String publisher, String isbn, Double price, Integer totalQty, Integer availableQty, String lastUpdateDate, String lastUpdateTime) {
-        this.bookId = bookId;
-        this.bookName = bookName;
-        this.author = author;
-        this.edition = edition;
-        this.publisher = publisher;
-        this.isbn = isbn;
-        this.price = price;
-        this.totalQty = totalQty;
-        this.availableQty = availableQty;
-        this.lastUpdateDate = lastUpdateDate;
-        this.lastUpdateTime = lastUpdateTime;
-    }
-
-    public BookDTO() {
-    }
-
-    public String getBookId() {
-        return bookId;
-    }
-
-    public void setBookId(String bookId) {
-        this.bookId = bookId;
-    }
-
-    public String getBookName() {
-        return bookName;
-    }
-
-    public void setBookName(String bookName) {
-        this.bookName = bookName;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public String getEdition() {
-        return edition;
-    }
-
-    public void setEdition(String edition) {
-        this.edition = edition;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
-
-    public Double getPrice() {
-        return price;
-    }
-
-    public void setPrice(Double price) {
-        this.price = price;
-    }
-
-    public Integer getTotalQty() {
-        return totalQty;
-    }
-
-    public void setTotalQty(Integer totalQty) {
-        this.totalQty = totalQty;
-    }
-
-    public Integer getAvailableQty() {
-        return availableQty;
-    }
-
-    public void setAvailableQty(Integer availableQty) {
-        this.availableQty = availableQty;
-    }
-
-    public String getLastUpdateDate() {
-        return lastUpdateDate;
-    }
-
-    public void setLastUpdateDate(String lastUpdateDate) {
-        this.lastUpdateDate = lastUpdateDate;
-    }
-
-    public String getLastUpdateTime() {
-        return lastUpdateTime;
-    }
-
-    public void setLastUpdateTime(String lastUpdateTime) {
-        this.lastUpdateTime = lastUpdateTime;
-    }
-
-    @Override
-    public String toString() {
-        return "BookDTO{" +
-                "bookId='" + bookId + '\'' +
-                ", bookName='" + bookName + '\'' +
-                ", author='" + author + '\'' +
-                ", edition='" + edition + '\'' +
-                ", publisher='" + publisher + '\'' +
-                ", isbn='" + isbn + '\'' +
-                ", price=" + price +
-                ", totalQty=" + totalQty +
-                ", availableQty=" + availableQty +
-                ", lastUpdateDate='" + lastUpdateDate + '\'' +
-                ", lastUpdateTime='" + lastUpdateTime + '\'' +
-                '}';
-    }
 }
