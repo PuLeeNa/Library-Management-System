a056e061bdfc608b42c5e5b9425b1dda04baf48e
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-06 20:59:59 +0530

    Add controllers for lending, members, and staff with appropriate request mappings

--- src/main/java/lk/puLeeNa/LibManagement/controller/LengdingController.java
+++ src/main/java/lk/puLeeNa/LibManagement/controller/LengdingController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/staffs")
-public class StaffController {
+@RequestMapping("/api/v1/lendings")
+public class LengdingController {
 }
