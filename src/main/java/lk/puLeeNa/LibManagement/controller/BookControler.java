2030172708af0735a3497edd6384aed9dbcea974
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-06 20:37:13 +0530

    Add getAllBooks endpoint to BookController for retrieving all books

--- src/main/java/lk/puLeeNa/LibManagement/controller/BookControler.java
+++ src/main/java/lk/puLeeNa/LibManagement/controller/BookControler.java
@@ -6,6 +6,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookControler {
@@ -47,5 +50,36 @@ public class BookControler {
                 "10:30:00"
         );
         return new ResponseEntity<>(bookDTO, HttpStatus.OK);
+    }
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAllBooks(){
+        List<BookDTO> bookDTOList = new ArrayList<>();
+        bookDTOList.add(new BookDTO(
+                "B001",
+                "Java Programming",
+                "Herbert Schildt",
+                "8th",
+                "McGraw-Hill Education",
+                "978-1260440232",
+                45.99,
+                10,
+                7,
+                "2023-10-01",
+                "10:30:00"
+        ));
+        bookDTOList.add(new BookDTO(
+                "B002",
+                "Effective Java",
+                "Joshua Bloch",
+                "3rd",
+                "Addison-Wesley Professional",
+                "978-0134686097",
+                54.99,
+                8,
+                5,
+                "2023-09-15",
+                "14:20:00"
+        ));
+        return new ResponseEntity<>(bookDTOList, HttpStatus.OK);
     }
 }
