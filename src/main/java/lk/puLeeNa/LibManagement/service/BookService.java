281dd1ef6e82f3571da247bbe2a1287dc9901fdb
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-07 11:07:12 +0530

    Add BookService interface and implementation for book management

---
+++ src/main/java/lk/puLeeNa/LibManagement/service/BookService.java
@@ -0,0 +1,13 @@
+package lk.puLeeNa.LibManagement.service;
+
+import lk.puLeeNa.LibManagement.dto.BookDTO;
+
+import java.util.List;
+
+public interface BookService {
+    void addBook(BookDTO bookDTO);
+    void deleteBook(String bookId);
+    void updateBook(String bookId, BookDTO bookDTO);
+    BookDTO getSelectedBook(String bookId);
+    List<BookDTO> getSelectedBooks();
+}
---
+++ src/main/java/lk/puLeeNa/LibManagement/service/impl/BookServiceIMPL.java
@@ -0,0 +1,36 @@
+package lk.puLeeNa.LibManagement.service.impl;
+
+import lk.puLeeNa.LibManagement.dto.BookDTO;
+import lk.puLeeNa.LibManagement.service.BookService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceIMPL implements BookService {
+
+    @Override
+    public void addBook(BookDTO bookDTO) {
+
+    }
+
+    @Override
+    public void deleteBook(String bookId) {
+
+    }
+
+    @Override
+    public void updateBook(String bookId, BookDTO bookDTO) {
+
+    }
+
+    @Override
+    public BookDTO getSelectedBook(String bookId) {
+        return null;
+    }
+
+    @Override
+    public List<BookDTO> getSelectedBooks() {
+        return List.of();
+    }
+}
