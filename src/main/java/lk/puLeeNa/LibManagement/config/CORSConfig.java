46e71f3242e6e64335b95f959c429a1d202c7207
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-10-22 23:28:05 +0530

    minor changes

--- src/main/java/lk/puLeeNa/LibManagement/config/CORSConfig.java
+++ src/main/java/lk/puLeeNa/LibManagement/config/CORSConfig.java
@@ -1,9 +1,13 @@
 package lk.puLeeNa.LibManagement.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CORSConfig {
+    @Bean
     public WebMvcConfigurer configurer() {
         return new WebMvcConfigurer() {
             @Override
