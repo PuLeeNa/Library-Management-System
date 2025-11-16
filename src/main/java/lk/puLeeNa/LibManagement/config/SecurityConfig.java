c91ff210278ab21843e4bb682a7619c65d5302e3
Your Name <wickramasooriyabgpv.22@uom.lk>
2025-11-17 01:36:45 +0530

    Update application properties for production environment and enhance CORS configuration

--- src/main/java/lk/puLeeNa/LibManagement/config/SecurityConfig.java
+++ src/main/java/lk/puLeeNa/LibManagement/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package lk.puLeeNa.LibManagement.config;
 import lk.puLeeNa.LibManagement.security.JwtRequestFilter;
 import lk.puLeeNa.LibManagement.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -68,10 +69,13 @@ public class SecurityConfig {
         return http.build();
     }
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // Your
+        configuration.setAllowedOrigins(Arrays.asList(frontendUrl,"http://localhost:3000")); // Your
                                                                                                           // frontend
                                                                                                           // URL
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT","PATCH", "DELETE", "OPTIONS"));
